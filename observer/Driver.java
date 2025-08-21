9823efad77f50528bc7db4208c396c21656475ff
XyuTheAscended <tyler.christian.norman@gmail.com>
2025-08-21 09:13:36 -0400

    I finally did it

--- observer/Driver.java
+++ observer/Driver.java
@@ -1,4 +1,4 @@
-package strategy;
+package observer;
 
 public class Driver {
     public static void main(String[] args) {
---
+++ strategy/King.java
@@ -0,0 +1,9 @@
+package strategy;
+
+public class King {
+    private String title;
+
+    public King(String title){
+        
+    }
+}
